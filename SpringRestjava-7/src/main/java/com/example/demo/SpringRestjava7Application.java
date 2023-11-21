856e2676927362b4674e1f4922d9c77770b7ed0f
nesravan <119496533+nesravan@users.noreply.github.com>
2023-11-21 21:57:37 +0530

    Add files via upload

---
+++ SpringRestjava-7/src/main/java/com/example/demo/SpringRestjava7Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestjava7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestjava7Application.class, args);
+	}
+
+}
