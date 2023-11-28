53c20e2e952e47d79696337f6b63d28e776dc96c
nesravan <119496533+nesravan@users.noreply.github.com>
2023-11-28 16:27:57 +0530

    Add files via upload

---
+++ springboot_microservices/department-service/department-service/src/main/java/com/cg/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,30 @@
+package com.cg.departmentservice.controller;
+
+import com.cg.departmentservice.dto.DepartmentDto;
+import com.cg.departmentservice.service.DepartmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/departments")
+@AllArgsConstructor
+public class DepartmentController {
+
+    private DepartmentService departmentService;
+
+    //build save department rest api
+    @PostMapping
+    public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto){
+        DepartmentDto savedDepartment = departmentService.saveDepartment(departmentDto);
+        return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
+    }
+
+    //build get department rest api
+    @GetMapping("{department-code}")
+    public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String departmentCode){
+        DepartmentDto departmentDto = departmentService.getDepartmentByCode(departmentCode);
+        return new ResponseEntity<>(departmentDto, HttpStatus.OK);
+    }
+}
