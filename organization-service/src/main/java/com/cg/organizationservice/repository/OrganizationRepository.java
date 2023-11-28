12d93fe000db4457c39b549c27ecd507f7b1769e
nesravan <119496533+nesravan@users.noreply.github.com>
2023-11-28 16:34:46 +0530

    Delete organization-service directory

--- organization-service/src/main/java/com/cg/organizationservice/repository/OrganizationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cg.organizationservice.repository;
-
-import com.cg.organizationservice.entity.Organization;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizationRepository extends JpaRepository<Organization, Long> {
-    Organization findByOrganizationCode(String organizationCode);
-}
