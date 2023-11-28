2acee6959a15dbcac7bbf8367a75b2e3ca36651f
nesravan <119496533+nesravan@users.noreply.github.com>
2023-11-28 16:30:07 +0530

    Add files via upload

---
+++ springboot_microservices/organization-service/src/main/java/com/cg/organizationservice/service/OrganizationService.java
@@ -0,0 +1,10 @@
+package com.cg.organizationservice.service;
+
+import com.cg.organizationservice.dto.OrganizationDto;
+
+public interface OrganizationService {
+
+    OrganizationDto saveOrganization(OrganizationDto organizationDto);
+
+    OrganizationDto getOrganizationByCode(String organizationCode);
+}
