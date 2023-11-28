af41f8cd2a17f9eff842c727bb513f3ef646831b
nesravan <119496533+nesravan@users.noreply.github.com>
2023-11-28 16:34:23 +0530

    Delete employee-service/employee-service directory

--- employee-service/employee-service/src/main/java/com/cg/employeeservice/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cg.employeeservice.repository;
-
-import com.cg.employeeservice.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
